bfe5a56f271437466653cc577de3a875da786f57
Moazzam <muhammadmoazzam143@gmail.com>
2024-06-09 11:16:39 +0500

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print('Ali\aliyan')
+
+
+# In[4]:
+
+
+a = 10
+id(a)
+
+
+# In[5]:
+
+
+a = 'Khan'
+id(a)
+
+
+# In[6]:
+
+
+b = 20
+id(b)
+
+
+# In[7]:
+
+
+a = 10
+id(a)
+
+
+# In[8]:
+
+
+a = 5+5
+id(a)
+
+
+# In[ ]:
+
+
+
+
